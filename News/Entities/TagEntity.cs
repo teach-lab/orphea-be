197e084ce5eda093be271cd6198342e8675ad0bb
EnjoyGray <vanya.konrad@gmail.com>
2024-09-26 19:37:01 +0200

    add models

--- News/Entities/TagEntity.cs
+++ News/Entities/TagEntity.cs
@@ -3,7 +3,7 @@
 public class TagEntity
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     public List<ArticleTagEntity>? ArticleTags { get; set;}
 }
