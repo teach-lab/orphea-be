a1ebc2e26648170b8550e1a97ab7e9ec146ecff9
EnjoyGray <vanya.konrad@gmail.com>
2024-09-26 19:37:01 +0200

    add models

---
+++ News/Entities/Models/PublisherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace News.Entities.Models
+{
+    public class PublisherModel
+    {
+        public string Name { get; set; }
+        public int TrustScore { get; set; }
+
+        public List<ArticleEntity> Articles { get; set; }
+    }
+}
