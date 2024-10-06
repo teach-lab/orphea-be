07e90b62f0dd82799c868af15186a7744bbd180e
EnjoyGray <vanya.konrad@gmail.com>
2024-10-06 10:27:22 +0200

    add id Guid to article models

--- News/Entities/Models/ArticleModel.cs
+++ News/Entities/Models/ArticleModel.cs
@@ -2,6 +2,7 @@
 
 public class ArticleModel
 {        
+    public Guid Id { get; set; }
     public string? Title { get; set; }   
     public string? SourceUrl { get; set; }
     public string? ImageUrl { get; set; }
