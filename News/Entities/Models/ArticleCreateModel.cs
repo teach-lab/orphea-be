29ae1b79bb9578fc85636d4fcce1c10a94323664
EnjoyGray <vanya.konrad@gmail.com>
2024-10-06 21:04:34 +0200

    fix AricleCreateModel - add datatime

--- News/Entities/Models/ArticleCreateModel.cs
+++ News/Entities/Models/ArticleCreateModel.cs
@@ -6,5 +6,6 @@ public class ArticleCreateModel
     public string? SourceUrl { get; set; }
     public string? ImageUrl { get; set; }
     public string? Description { get; set; }
+    public DateTime PublishedAt { get; set; }
     public Guid? PublisherId { get; set; }
 }
