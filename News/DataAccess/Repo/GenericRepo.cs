0580d06fe2676693c884f16c8ea7cc6c46cf1442
EnjoyGray <vanya.konrad@gmail.com>
2024-10-26 11:16:14 +0200

    add generic CRUD & save method for repo

---
+++ News/DataAccess/Repo/GenericRepo.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using News.DataAccess.Repo.RepoInterfaces;
+
+namespace News.DataAccess.Repo;
+
+public class GenericRepo<T> : IGenericRepo<T> where T : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepo(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    public async Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbSet.FindAsync(new object[] { id }, cancellationToken);
+    }
+
+    public async Task<T> UpdateAsync(T entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    public async Task<T> DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var entity = await GetByIdAsync(id, cancellationToken);
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+    public async Task SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ News/DataAccess/Repo/RepoInterfaces/IGenericRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace News.DataAccess.Repo.RepoInterfaces;
+
+public interface IGenericRepo<T> where T : class
+{
+    Task<T> CreateAsync(T entity, CancellationToken cancellationToken);
+
+    Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<T> UpdateAsync(T entity, CancellationToken cancellationToken);
+
+    Task<T> DeleteAsync(Guid id, CancellationToken cancellationToken);
+
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
