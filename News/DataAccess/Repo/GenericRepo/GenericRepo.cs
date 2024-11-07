92aa57db709fd32fe3edeb6f22eccaa3ec592f37
EnjoyGray <vanya.konrad@gmail.com>
2024-11-07 17:50:21 +0100

    add negative test, need fix

--- News/DataAccess/Repo/GenericRepo/GenericRepo.cs
+++ News/DataAccess/Repo/GenericRepo/GenericRepo.cs
@@ -24,7 +24,14 @@ public class GenericRepo<T> : IGenericRepo<T> where T : BaseEntity
 
     public async Task<T> GetByIdAsync(Guid? id, CancellationToken cancellationToken)
     {
-        return await _dbSet.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+        var result = await _dbSet.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+
+        if (result is null)
+        {
+            // TODO: Add not found exception
+            throw new Exception("Entity not found");
+        }
+        return result;
     }
 
     public async Task<T> UpdateAsync(T entity, CancellationToken cancellationToken)
